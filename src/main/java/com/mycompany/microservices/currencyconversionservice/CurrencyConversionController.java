7a435e71799e063eab9d8bcd5f05b065d4c75f08
vjpal3 <vrishalipal@yahoo.com>
2020-05-04 19:51:59 -0400

    Use Feign REST Client for Service Invocation

--- src/main/java/com/mycompany/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/mycompany/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,20 @@ import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired  
+	private CurrencyExchangeServiceProxy proxy;  
+	
+	@GetMapping("/currency-converter-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)  {
+		
+		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to); 
+		
+		//creating a new response bean
+		//getting the response back and taking it into Bean
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), 
+				quantity, quantity.multiply(response.getConversionMultiple()), response.getPort());
+	}
 	
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity)  
---
+++ src/main/java/com/mycompany/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -0,0 +1,12 @@
+package com.mycompany.microservices.currencyconversionservice;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="currency-exchange-service", url="localhost:8000")
+public interface CurrencyExchangeServiceProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
+	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
+}
