b383c33c783b24f855c6424970d01c37875991fa
vjpal3 <vrishalipal@yahoo.com>
2020-05-08 00:14:30 -0400

    Enable Zuul API Gateway

--- src/main/java/com/mycompany/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/mycompany/microservices/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -6,10 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange-service", url="localhost:8000")
-@FeignClient(name="currency-exchange-service")
+//@FeignClient(name="currency-exchange-service")
+
+//Enabling Zuul server
+@FeignClient(name="netflix-zuul-api-gateway-server")
 @RibbonClient(name="currency-exchange-service")  
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
+	//mapping for currency-exchange-service
+	// @GetMapping("/currency-exchange/from/{from}/to/{to}"
+	
+	//mapping for zuul-api-gateway-server
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")		//where {from} and {to} are path variable
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to); //from map to USD and to map to INR
 }
